149e16d241a0f4bdc0dd2c3891dc1d75852ed599
Eternal-15 <shakyasambad5@gmail.com>
2026-07-22 07:47:13 +0545

    Fixed budget and goal

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import expenses, incomes, reports, goals, recurring
+from routers import expenses, incomes, reports, goals, recurring, budget, import_statement
 from JWT_Authentication import auth
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Mero Kharcha",
     version="1.0.0",
-    description="Personal expense tracking system. Manage expenses, incomes, budgets, and department-wise reports. Built with FastAPI and PostgreSQL.",
+    description="Personal expense tracking system. Manage expenses, incomes, budgets, and category-wise reports. Built with FastAPI and PostgreSQL.",
 )
 
 origin = [
@@ -27,9 +27,10 @@ app.include_router(auth.router_login, prefix="/auth", tags=["Auth"])
 app.include_router(expenses.router_expense, prefix="/expenses", tags=["Expenses"])
 app.include_router(incomes.router_income, prefix="/incomes", tags=["Incomes"])
 app.include_router(goals.router_goals, prefix="/goals", tags=["Goals"])
-app.include_router(reports.router_reports, prefix="/reports", tags=["Reports"])
+app.include_router(import_statement.router_imports, prefix="/imports", tags=["Imports"])
 app.include_router(recurring.router_recurring, prefix="/recurring", tags=["Recurring"])
-
+app.include_router(budget.router_budget, prefix="/budget", tags=["Budget"])
+app.include_router(reports.router_reports, prefix="/reports", tags=["Reports"])
 
 @app.get("/")
 def read_root():
